dc94d00320e6adbf89762a821e78ac9e2cd40156
VetechStar <ghostdedsec8@gmail.com>
2024-05-28 01:16:51 +0700

    Alpha
    Baik pengembangan aplikasi di Ionic maupun di android studio tetap menghadapi masalah. Selain kendala yang sama dihadapi pada pengembangan aplikasi konversi, terdapat beberapa masalah yang masih belum dapat diresolusi. Diantaranya:
    -Permasalahan API
    -Aplikasi sering force close saat pengujian di emulator

---
+++ app/src/main/java/com/example/newsappv1/HomeActivity.java
@@ -0,0 +1,39 @@
+package com.example.newsappv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getNews();
+    }
+
+    void getNews(){
+        NewsApiClient newsApiClient = new NewsApiClient("86774e5d2e584d93a44a4ac195df911c");
+        newsApiClient.getTopHeadlines(
+                new TopHeadlinesRequest.Builder()
+                        .language("en")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        Log.i("GOT RESPONSE",response.toString());
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.i("GOT Failure",throwable.getMessage());
+                    }
+                }
+        );
+    }
+}
